be2b0bf2e037e5ef2d546e3f161544d6479a44e6
Vinh Tran <vinhtran2905@gmail.com>
2018-04-27 21:38:51 -0500

    Maven with test NG demo

---
+++ src/main/java/MavenwithTestNGDemo/MavenwithTestNGDemo/App.java
@@ -0,0 +1,13 @@
+package MavenwithTestNGDemo.MavenwithTestNGDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/qa/test/TestNGDemo.java
@@ -0,0 +1,41 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+	public class DemoTestNG {
+		@Test
+		public void sum() {
+			System.out.println("SUM");
+			int a = 20;
+			int b = 10;
+			Assert.assertEquals(30, a + b);
+		}
+
+		@Test
+		public void sub() {
+			System.out.println("SUB");
+			int a = 20;
+			int b = 10;
+			Assert.assertEquals(10, a - b);
+		}
+
+		@Test
+		public void div() {
+			System.out.println("DIV");
+			int a = 20;
+			int b = 10;
+			Assert.assertEquals(2, a / b);
+		}
+
+		@Test
+		public void mul() {
+			System.out.println("MUL");
+			int a = 20;
+			int b = 10;
+			Assert.assertEquals(200, a * b);
+		}
+	}
+
+}
